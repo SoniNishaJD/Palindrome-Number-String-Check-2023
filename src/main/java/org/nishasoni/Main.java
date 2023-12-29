6e2186fa19037fee2f220ab3c1b8a10717fc40d5
Soni Nisha <soni.nishaa24@gmail.com>
2023-12-29 22:47:53 +0530

    Palindrom program

---
+++ src/main/java/org/nishasoni/Main.java
@@ -0,0 +1,24 @@
+package org.nishasoni;
+
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("///////Palindrom///////////String And Number////////////");
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        String rev="";
+        for(int i=s.length()-1;i>=0;i--){
+            char c=s.charAt(i);
+            rev+=c;
+        }
+        if(s.equals(rev)){
+            System.out.print("YES");
+            return;
+        }
+        else{
+            System.out.print("NO");
+        }
+    }
+}
